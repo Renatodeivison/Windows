78d701edd68606e578d9175e9d019bdb54f47e6c
Renato Deivison <renato.deivison@gmail.com>
2015-07-07 10:57:01 -0700

    Monday Updates
    MavLink packet decode

--- Test/main.cpp
+++ Test/main.cpp
@@ -6,17 +6,20 @@
  */
 
 #include <cstdlib>
-
-#include "C:\Users\Station1\Desktop\c_uart_interface_example-master\mavlink_control.h"
-#include "C:\Users\Station1\Desktop\c_uart_interface_example-master\serial_port.h"
-#include "C:\Users\Station1\Desktop\c_uart_interface_example-master\autopilot_interface.h"
-
+#include <stdio.h>
 #include <winsock2.h>
 #include <string>
+#include <iostream>
+
+#include "C:\Users\Station1\Desktop\mavlink-master\out\common\mavlink.h"
+#include "C:\Users\Station1\Desktop\mavlink-master\out\mavlink_helpers.h"
+#include "C:\Users\Station1\Desktop\mavlink-master\out\protocol.h"
+#include "C:\Users\Station1\Desktop\mavlink-master\out\mavlink_types.h"
+
 
 #define SERVER "127.0.0.1"  //ip address of udp server
 #define BUFLEN 283  //Max length of buffer
-#define SouPORT 14550 
+#define SouPORT 56000
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
@@ -26,7 +29,7 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    
     WSADATA wsa;
     SOCKET s;
     struct sockaddr_in server;
@@ -34,9 +37,12 @@ int main(int argc, char** argv) {
     char buf[BUFLEN];
     char *message;
     slen = sizeof(server);
-    
-  // mavlink_local_position_ned_t local_position_ned;
-    
+    mavlink_message_t msg;
+    const mavlink_message_t* msg1;
+    mavlink_status_t status;
+    unsigned int temp = 0;
+    ssize_t receive;   
+    mavlink_attitude_t attitude;
     
     //Prepare the sockaddr_in structure
     memset((char *) &server, 0, sizeof(s));
@@ -61,32 +67,49 @@ int main(int argc, char** argv) {
     printf("Socket created.\n");
     
     //Bind
-    if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+   if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
     {
         printf("Bind failed with error code : %d" , WSAGetLastError());
     }
-    
+    printf("Bind done.\n");
     //Send some data
     while(1){
-        message = "Olahi";
-       /* Sleep(1000);
-        
-        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &server, slen) == SOCKET_ERROR)
-        {
-            printf("sendto() failed with error code : %d" , WSAGetLastError());
-            exit(EXIT_FAILURE);
-        }
-        */
+       // message = "Olahi";
+        // Sleep(1000);
+         
+        // if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &server, slen) == SOCKET_ERROR)
+         //{
+        // printf("sendto() failed with error code : %d" , WSAGetLastError());
+        // exit(EXIT_FAILURE);
+        // }
+        // puts(buf);
+         
         //receive a reply and print it
         //clear the buffer by filling null, it might have previously received data
         memset(buf,'\0', BUFLEN);
         
         //try to receive some data, this is a blocking call
-        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &server, &slen) == SOCKET_ERROR)
+        receive = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &server, &slen);
+        if (receive == SOCKET_ERROR)
         {
             printf("recvfrom() failed with error code : %d" , WSAGetLastError());
             exit(EXIT_FAILURE);
-        }  
+        }
+        
+        cout << "Bytes Received: " << (int)receive << "\nDatagram:\n";
+        for (int i = 0; i < receive; ++i)
+        {
+            temp = buf[i];
+//            printf("%02x ", (unsigned char)temp);
+            if (mavlink_parse_char(MAVLINK_COMM_0, buf[i], &msg, &status))
+            {
+//                printf("\nReceived packet: SYS: %u, COMP: %u, LEN: %u, MSG ID: %u", msg.sysid, msg.compid, msg.len, msg.msgid);
+//                cout << " Payload: " << msg.payload64 << " \n\n";
+                mavlink_msg_attitude_encode(1, 200, &msg, &attitude);
+                cout << "Yaw: " << attitude.yaw << " Pitch:" << attitude.pitch << "Roll: " << attitude.roll << endl;
+              
+            }
+        }
     }
     
     puts(buf);
